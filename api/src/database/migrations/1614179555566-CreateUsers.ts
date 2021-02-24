2ec801efe2d861a233a9b648a148de6987a42a6d
Tiago Almeida <tiagovdaa@gmail.com>
2021-02-24 12:14:28 -0300

    migration first run

---
+++ api/src/database/migrations/1614179555566-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1614179555566 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
