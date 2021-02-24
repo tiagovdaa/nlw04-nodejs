e1f2573f4ff489c97f0d16203aac17dcbde648dc
Tiago Almeida <tiagovdaa@gmail.com>
2021-02-24 12:55:45 -0300

    criado rotas e testado envio de json pelo insomnia

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,29 +1,11 @@
 import 'reflect-metadata';
 import express from 'express';
 import "./database";
+import { router } from "./routes"
 
 const app = express();
 
-/**
- * GET => Busca
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH = <> Alteração Específica
- */
-
-// http://localhost:3333/users
-
-app.get("/", (request, response) => {
-    return response.json({ "message": "Hello World" });
-});
-
-// 1 param => Rota(Recurso API)
-// 2 param => request, response
-
-app.post("/", (request, response) => {
-    // Recebeu os dados para salvar
-    return response.json({ message: "Os dados foram salvos com sucesso!" })
-})
+app.use(express.json());
+app.use(router)
 
 app.listen(3333, () => console.log("Server is running!"));
