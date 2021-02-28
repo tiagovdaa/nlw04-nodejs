71af97e0bea4e6f09135e08da42f73f3609d855f
Tiago Almeida <tiagovdaa@gmail.com>
2021-02-28 17:07:43 -0300

    entity created.

--- api/src/models/User.ts
+++ api/src/models/User.ts
@@ -1,8 +1,25 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryColumn, CreateDateColumn } from "typeorm";
+import { v4 as uuid } from 'uuid';
 
-@Entity
+@Entity("users")
 class User {
 
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id)
+            this.id = uuid()
+    }
 }
 
 export { User }
